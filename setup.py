246055df358ea0961a77be209837c6a5f5daa400
Tom Viner <tom@viner.tv>
2020-05-24 21:15:48 +0100

    proper python package

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='red-dead-tools',
-    packages=['red_dead'],
+    version="0.1.0",
+    packages=find_packages(),
+    url="https://github.com/red-dead-tools/red-dead-tools",
+    description="Tools for playing Red Dead Redemption 2 online",
+    author="Tom Viner",
+    author_email="red-dead-tools@viner.tv",
+    install_requires=[
+        'gspread',
+        'attrs',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
